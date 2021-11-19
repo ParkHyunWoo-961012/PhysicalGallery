6e2d8bec057f86f0d4e4e725f11aea88d2543ff9
ParkHyunWoo-961012 <59594861+ParkHyunWoo-961012@users.noreply.github.com>
2021-11-19 11:28:09 +0900

    Editied by 11/15

--- app/src/main/java/com/example/physicalgallery/Login.kt
+++ app/src/main/java/com/example/physicalgallery/Login.kt
@@ -4,22 +4,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.physicalgallery.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSignIn : GoogleSignInClient? =null
+    var GOOGLE_LOGIN_CODE = 1007
+    val binding by lazy{ActivityLoginBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
-        email_login_button.setOnClickListener {
+        setContentView(binding.root)
+        binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
+        }
+        binding.googleSignButton.setOnClickListener{
+            googlelogin()
+        }
+
+        var google = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("361991051449-d13a6j16rk7j85o6pdb8jucub35kqgk5.apps.googleusercontent.com").requestEmail().build()
+        googleSignIn = GoogleSignIn.getClient(this,google)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account = result.signInAccount
+                firebaseAuthGoogle(account)
+            }
+        }
+    }
+    fun firebaseAuthGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+    }
+    fun googlelogin(){
+        var GoogleIntent = googleSignIn?.signInIntent.run{
+            startActivityForResult(this,GOOGLE_LOGIN_CODE)
         }
     }
     fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(),binding.passwordEdittext.text.toString())
             ?.addOnCompleteListener {
                     task ->
                 if(task.isSuccessful){
@@ -35,7 +72,7 @@ class Login : AppCompatActivity() {
             }
     }
     fun signinEmail(){
-        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(),binding.passwordEdittext.text.toString())
             ?.addOnCompleteListener {
                     task ->
                 if(task.isSuccessful){
--- app/src/main/java/com/example/physicalgallery/MainActivity.kt
+++ app/src/main/java/com/example/physicalgallery/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.physicalgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.physicalgallery.databinding.ActivityMainBinding
+import com.example.physicalgallery.navigation.AlarmFrag
+import com.example.physicalgallery.navigation.DetailFrag
+import com.example.physicalgallery.navigation.GridFrag
+import com.example.physicalgallery.navigation.UserFrag
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home ->{
+                var detailFrag = DetailFrag()
+                supportFragmentManager.beginTransaction().replace(R.id.main_contents,detailFrag).commit()
+                return true
+            }
+            R.id.account ->{
+                var userFrag = UserFrag()
+                supportFragmentManager.beginTransaction().replace(R.id.main_contents,userFrag).commit()
+                return true
+            }
+            R.id.upload ->{
+                var detailFrag = DetailFrag()
+                supportFragmentManager.beginTransaction().replace(R.id.main_contents,detailFrag).commit()
+                return true
+            }
+            R.id.search->{
+                var gridFrag = GridFrag()
+                supportFragmentManager.beginTransaction().replace(R.id.main_contents,gridFrag).commit()
+                return true
+            }
+            R.id.alarm ->{
+                var alarmFrag = AlarmFrag()
+                supportFragmentManager.beginTransaction().replace(R.id.main_contents,alarmFrag).commit()
+                return true
+            }
+        }
     }
 }
