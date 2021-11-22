3fddf51c490cf40483dae76ec3ae2e5bfadd0464
ParkHyunWoo-961012 <59594861+ParkHyunWoo-961012@users.noreply.github.com>
2021-11-22 19:47:56 +0900

    Food Search DB construct

--- app/src/main/java/com/example/physicalgallery/FoodSearchActivity.kt
+++ app/src/main/java/com/example/physicalgallery/FoodSearchActivity.kt
@@ -1,42 +1,55 @@
 package com.example.physicalgallery
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.physicalgallery.databinding.ActivityFoodSearchBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
 import org.w3c.dom.Text
 import java.io.BufferedInputStream
 
 class FoodSearchActivity : AppCompatActivity() {
-    val FoodDB : FoodDatabase by lazy {FoodDatabase.getInstance(this)!!}
-    var name = "돼지고기"
+    val FoodDB: FoodDatabase by lazy { FoodDatabase.getInstance(this)!! }
+    val binding by lazy{ActivityFoodSearchBinding.inflate(layoutInflater)}
+    lateinit var food: FoodDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityFoodSearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.e("FoodList", "음식데이터 조회.")
-        val getFood = GetFood(applicationContext)
-        getFood.start()
 
-    }
-    fun findfood(view: View){
-        GlobalScope.launch(Dispatchers.IO){
-            val foodList : List<Food> = FoodDB.FoodDao().getItem(name)
-            Log.d("112323","${foodList}")
+        binding.foodSearch.setOnClickListener{
+            var name = (binding.foodName.text).toString()
+            if (name == ""){
+                Toast.makeText(applicationContext,"you don't any input",Toast.LENGTH_LONG).show()
+            }
+            else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    var A = FoodDB.FoodDao().getItem(name)
+                    if (A[0] != null){
+
+                        for(i in 0 .. (A.size-1)) {
+                            Log.e("Food Result", "${A[i].food_name}")
+                        }
+                    }
+                }
+            }
         }
     }
-}
-class GetFood(val context: Context) : Thread(){
-    override fun run() {
-        val items = FoodDatabase
-            .getInstance(context)!!
-            .FoodDao()
-            .getItem(name)
-       Log.d("123123123","${name.toString()}")
-    }
+
+//    fun find_food(view: View) {
+//        var name:String? = binding.foodName.text as String?
+//        if (name == null){
+//            Toast.makeText(applicationContext,"you don't any input",Toast.LENGTH_LONG).show()
+//        }
+//        else {
+//            CoroutineScope(Dispatchers.IO).launch {
+//                var A = FoodDB?.FoodDao().getItem(name)
+//                Log.e("11","${A.toString()}")
+//            }
+//        }
+//    }
 }
